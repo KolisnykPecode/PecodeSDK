1ba4adef683a819d98937336e53eec3f66bb8e1f
Andrew Kolisnyk <andrew_kolisnyk@pecodesoftware.com>
2024-04-23 21:36:44 +0300

    added firebase manager

---
+++ PecodeSDK/FirebaseManager.swift
@@ -0,0 +1,15 @@
+//
+//  FirebaseManager.swift
+//  PecodeSDK
+//
+//  Created by kolisnyk9 on 23/04/2024.
+//
+
+import Foundation
+
+public class FirebaseManager {
+    
+    public func printText() {
+        print("text")
+    }
+}
