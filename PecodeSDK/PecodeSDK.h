a9516ba24e364a9465f7d89caa5151aa2c04b51b
Andrew Kolisnyk <andrew_kolisnyk@pecodesoftware.com>
2024-04-23 21:30:22 +0300

    Initial Commit

---
+++ PecodeSDK/PecodeSDK.h
@@ -0,0 +1,18 @@
+//
+//  PecodeSDK.h
+//  PecodeSDK
+//
+//  Created by kolisnyk9 on 23/04/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PecodeSDK.
+FOUNDATION_EXPORT double PecodeSDKVersionNumber;
+
+//! Project version string for PecodeSDK.
+FOUNDATION_EXPORT const unsigned char PecodeSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PecodeSDK/PublicHeader.h>
+
+
